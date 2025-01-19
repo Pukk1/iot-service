92e3f2652bd648a97266584c436dae24d43b61ab
mobest1an <erikk202@gmail.com>
2025-01-19 19:57:54 +0300

    add elk

--- rule-engine/src/main/java/com/iver/ruleengine/service/Logger.java
+++
@@ -1,6 +0,0 @@
-package com.iver.ruleengine.service;
-
-public interface Logger {
-
-    void log(String message);
-}
--- rule-engine/src/main/java/com/iver/ruleengine/service/impl/RuleServiceImpl.java
+++ rule-engine/src/main/java/com/iver/ruleengine/service/impl/RuleServiceImpl.java
@@ -5,8 +5,9 @@ import com.iver.ruleengine.model.DevicePackage;
 import com.iver.ruleengine.model.RuleCheck;
 import com.iver.ruleengine.repository.RuleChecksRepository;
 import com.iver.ruleengine.rules.Rule;
-import com.iver.ruleengine.service.Logger;
 import com.iver.ruleengine.service.RuleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +18,11 @@ public class RuleServiceImpl implements RuleService {
 
     private final Map<String, Rule> rules;
     private final RuleChecksRepository ruleChecksRepository;
-    private final Logger logger;
+    private static final Logger logger = LoggerFactory.getLogger(RuleServiceImpl.class);
 
     @Autowired
-    public RuleServiceImpl(RuleChecksRepository ruleChecksRepository, Logger logger, Map<String, Rule> rules) {
+    public RuleServiceImpl(RuleChecksRepository ruleChecksRepository, Map<String, Rule> rules) {
         this.ruleChecksRepository = ruleChecksRepository;
-        this.logger = logger;
         this.rules = rules;
     }
 
@@ -32,7 +32,7 @@ public class RuleServiceImpl implements RuleService {
             boolean result = v.checkRule(devicePackage, data);
             RuleCheck ruleCheck = new RuleCheck(devicePackage.getDeviceId(), k, result);
             ruleChecksRepository.save(ruleCheck);
-            logger.log(k + " " + result);
+            logger.info("{} result is {}", k, result);
         });
     }
 }
