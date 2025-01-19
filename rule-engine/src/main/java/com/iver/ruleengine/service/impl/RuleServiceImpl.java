cbc6210fe1b8a77e23261a8859f91e72d79cb73d
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2025-01-19 19:06:32 +0300

    fix graph

--- rule-engine/src/main/java/com/iver/ruleengine/service/impl/RuleServiceImpl.java
+++ rule-engine/src/main/java/com/iver/ruleengine/service/impl/RuleServiceImpl.java
@@ -7,7 +7,11 @@ import com.iver.ruleengine.repository.RuleChecksRepository;
 import com.iver.ruleengine.rules.Rule;
 import com.iver.ruleengine.service.Logger;
 import com.iver.ruleengine.service.RuleService;
+import io.micrometer.common.util.StringUtils;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -18,9 +22,12 @@ public class RuleServiceImpl implements RuleService {
     private final Map<String, Rule> rules;
     private final RuleChecksRepository ruleChecksRepository;
     private final Logger logger;
+    private final MeterRegistry meterRegistry;
 
     @Autowired
-    public RuleServiceImpl(RuleChecksRepository ruleChecksRepository, Logger logger, Map<String, Rule> rules) {
+    public RuleServiceImpl(RuleChecksRepository ruleChecksRepository, Logger logger, Map<String, Rule> rules,
+            MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
         this.ruleChecksRepository = ruleChecksRepository;
         this.logger = logger;
         this.rules = rules;
@@ -32,6 +39,9 @@ public class RuleServiceImpl implements RuleService {
             boolean result = v.checkRule(devicePackage, data);
             RuleCheck ruleCheck = new RuleCheck(devicePackage.getDeviceId(), k, result);
             ruleChecksRepository.save(ruleCheck);
+            if (ruleCheck.getResult()) {
+                meterRegistry.counter("trust_result").increment();
+            }
             logger.log(k + " " + result);
         });
     }
